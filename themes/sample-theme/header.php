cb799a8bb4135617cbc3d3ebc6df07347c354a41
Jeet Sangani <jeetsangani@gmail.com>
2020-02-18 12:52:50 -0500

    Removeing unnecessary links from the main page

--- themes/sample-theme/header.php
+++ themes/sample-theme/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,37 +23,33 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sample-theme' ); ?></a>
+	<div id="page" class="site">
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
 				<?php
-			else :
+				the_custom_logo();
+				if (is_front_page() && is_home()) :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 				<?php
-			endif;
-			$sample_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $sample_theme_description || is_customize_preview() ) :
+				else :
 				?>
-				<p class="site-description"><?php echo $sample_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+				<?php
+				endif;
+				$sample_theme_description = get_bloginfo('description', 'display');
+				if ($sample_theme_description || is_customize_preview()) :
+				?>
+					<p class="site-description"><?php echo $sample_theme_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="navigation">
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'primary',
-				'menu_class'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			<nav id="site-navigation" class="navigation">
+				<?php
+				wp_nav_menu(array('theme_location' => 'primary', 'container_class' => 'nav-menu', 'menu_class' => 'nav-menu'));
+				?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
