343fbb6f4c817ab0e56a753f2df1224847c9bdcd
Jeet Sangani <jeetsangani@gmail.com>
2020-02-18 12:52:28 -0500

    Change in footer file

--- themes/sample-theme/footer.php
+++ themes/sample-theme/footer.php
@@ -15,13 +15,6 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sample-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'sample-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sample-theme' ), 'sample-theme', '<a href="https://jeetsangani.com/">Jeet Sangani</a>' );
