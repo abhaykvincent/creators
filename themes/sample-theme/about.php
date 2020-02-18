32284e91180fdcbefe46e9a0a26c7844a28fce07
Abhay Vincent <abhaykvincent@gmail.com>
2020-02-17 21:25:00 -0500

    About page structure created

---
+++ themes/sample-theme/about.php
@@ -0,0 +1,24 @@
+<?php /* Template Name: About*/get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+            <div class="home-banner"><img src="<?php bloginfo('template_url')?>/assets/img/home-banner.png" alt=""></div>
+            <div class="home-main">
+                <div class="home-intro">
+
+                    <h1>About Us</h1>
+
+                    <div class="intro-img"><img src="<?php bloginfo('template_url')?>/assets/img/intro-img.jpg" alt=""></div>
+                    <p>We are MaxFitness. Your transformation is our passion. We are your personal trainer, nutritionst, your suppliment expert, your lifting partner. We provide the tools, and products you need to burn fat, build muscle, and become your best self</p>
+                </div>
+            </div>
+
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();?>
