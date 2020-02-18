b8371118fe85d7b250b4c3d28e69124ab0fb7b3f
Abhay Vincent <abhaykvincent@gmail.com>
2020-02-17 21:13:28 -0500

    Home Page Structure created

--- themes/sample-theme/home.php
+++ themes/sample-theme/home.php
@@ -3,20 +3,35 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+            <div class="home-banner"><img src="<?php bloginfo('template_url')?>/assets/img/home-banner.png" alt=""></div>
+            <div class="home-main">
+                <div class="home-intro">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+                    <h1>Max Fitness</h1>
 
-			get_template_part( 'template-parts/content', 'page' );
+                    <div class="intro-img"><img src="<?php bloginfo('template_url')?>/assets/img/intro-img.jpg" alt=""></div>
+                    <p>We are MaxFitness. Your transformation is our passion. We are your personal trainer, nutritionst, your suppliment expert, your lifting partner. We provide the tools, and products you need to burn fat, build muscle, and become your best self</p>
+                </div>
+            </div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+            <div class="products">
+                <h1>Products</h1>
+                    <div class="product"></div>
+                    <div class="product"></div>
+                    <div class="product"></div>
+                    <div class="product"></div>
+            </div>
+            <div class="homeBottomContainer">
+                <div class="customer">
+                    <h1>Customers Come First</h1>
+                    <p>Our single-minded focus is helping you achieve your goals. If your motivation is flagging, we've got your back. We're obsessed with creating the best fitness site out there because we love hearing about your success once you’ve implemented our fitness solutions.</p>
+                </div>
+                <div class="offer">
+                    <h1> 20% Off on First Purchase!</h1>
+                    <p>Get upto 20% on Capsule, Pill, Liquid from brands like  Doctor's Best, NOW, Irwin Naturals</p>
+                </div>
+            </div>
+		
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
